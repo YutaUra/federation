2581856a6a63f2c6718ac1684aafe8b56ebd955c
David Glasser <glasser@davidglasser.net>
2022-08-05 23:09:58 -0700

    Change dependency on Apollo Server 3 to new type package
    This removes the last dependency on Apollo Server from Apollo Gateway.
    
    Full description to come.

--- gateway-js/src/__tests__/gateway/reporting.test.ts
+++ gateway-js/src/__tests__/gateway/reporting.test.ts
@@ -10,14 +10,14 @@ import { createHttpLink } from '@apollo/client/link/http';
 import fetch from 'node-fetch';
 import { ApolloGateway } from '../..';
 import { Plugin, Config, Refs } from 'pretty-format';
-import { Report, Trace } from 'apollo-reporting-protobuf';
+import { Report, Trace } from '@apollo/usage-reporting-protobuf';
 import { fixtures } from 'apollo-federation-integration-testsuite';
 import { nockAfterEach, nockBeforeEach } from '../nockAssertions';
 import { GraphQLSchemaModule } from '@apollo/subgraph/src/schema-helper';
 import resolvable, { Resolvable } from '@josephg/resolvable';
 
 // Normalize specific fields that change often (eg timestamps) to static values,
-// to make snapshot testing viable.  (If these helpers are more generally
+// to make snapshot testing viable.  (If tFhese helpers are more generally
 // useful, they could be moved to a different file.)
 
 const alreadyProcessed = '__already_processed__';
